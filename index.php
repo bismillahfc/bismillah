4cf3c3c03db601d42e15402da88cdbefba9e8776
bismillahfc <bismillahfootballclub@gmail.com>
2025-05-06 11:32:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// index.php
+header("Content-Type: application/json");
+
+// Inoltra richiesta a Notion
+$url = 'https://api.notion.com/v1/databases/1a4d83a736578092b23df8fb2199b419/query';
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(["page_size" => 1]));
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer ntn_tua_api_key_notion',
+    'Notion-Version: 2022-06-28',
+    'Content-Type: application/json'
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
